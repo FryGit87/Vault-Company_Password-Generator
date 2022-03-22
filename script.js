72df9a79ce50c54d2759c811429cf7fba381994d
Kym Reilly <kymreilly@hotmail.com>
2022-03-22 22:07:26 +1030

    User Input selection completed, need Math still

--- script.js
+++ script.js
@@ -20,14 +20,13 @@ var specialChar;
 var minLength = 8;
 var maxLength = 128;
 var pwLength;
-
-
+// var pwUserReq= "";
 
 
 function starterInput (){
     pwLength = parseInt(prompt( 'How long would you like your password to be?', 'Min: 8  -  Max: 128'), 10);
   if (isNaN(pwLength)){
-    alert('Use only numbers please');
+    alert('Use only numbers please.');
     starterInput ()
 } else if (pwLength < 8 || pwLength > 128) {
     alert('Please select a another number! (Min: 8  -  Max: 128)');
@@ -45,54 +44,44 @@ starterInput ();
 console.log(pwLength);
 
 function passwordConditions(){
+  var pwUserReq = "";
   lowercase = confirm("Would you like it to contain Lowercase?")
   uppercase = confirm("Would you like it to contain Uppercase?")
   numeric = confirm("Would you like it to contain Numbers?")
   specialChar = confirm("Would you like it to contain Special Characters?")
-
+    
+    if (lowercase){
+      pwUserReq += 'abcdefghijklmnopqrstuvwxyz'
+    }
+    if (uppercase){
+      pwUserReq += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    }
+    if (numeric){
+      pwUserReq += '0123456789'
+    }
+    if (specialChar){
+      pwUserReq += '!@#$%^&*";'
+    }
+  
   if (!lowercase && !uppercase && !numeric && !specialChar){
-  alert('Please select at least one of the criteria')
+  alert('Please select at least ONE of the password criteria.')
   passwordConditions()
-    // console.log(lowercase);
-    // console.log(uppercase);
-    // console.log(numeric);
-    // console.log(specialChar);
+    
+  }
+  return pwUserReq;
 }
-}
+
+
+    
+var output = passwordConditions();
+
     console.log(lowercase);
     console.log(uppercase);
     console.log(numeric);
     console.log(specialChar);
-passwordConditions();
 
-// //Call the password conditons function to get the user input
-// passwordConditions();
+    console.log(output);
 
 
 
 
-
-
-
-
-
-// function makeid(length) {
-//     var result           = '';
-//     var characters       = '!@#$%^&*';
-//     var charactersLength = characters.length;
-//     for ( var i = 0; i < length; i++ ) {
-//       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//    }
-//    return result;
-// }
-
-// console.log(makeid(length));
-
-
-
-
-
-// var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-// var lowercase = 'abcdefghijklmnopqrstuvwxyz'
-// var numeric = '0123456789'
-//var symbol = "!@#$%^&*";"
