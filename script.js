e152814edebf10e77aed22e299177669ee8edbbd
Kym Reilly <kymreilly@hotmail.com>
2022-03-23 21:08:36 +1030

    input validation working, need to concatenate

--- script.js
+++ script.js
@@ -20,48 +20,93 @@ var specialChar;
 var minLength = 8;
 var maxLength = 128;
 var pwLength;
-// var pwUserReq= "";
+var pwUserReq= "";
 
 
 function starterInput (){
     pwLength = parseInt(prompt( 'How long would you like your password to be?', 'Min: 8  -  Max: 128'), 10);
-  if (isNaN(pwLength)){
+ if (isNaN(pwLength)){
     alert('Use only numbers please.');
     starterInput ()
 } else if (pwLength < 8 || pwLength > 128) {
     alert('Please select a another number! (Min: 8  -  Max: 128)');
     starterInput ();
-// }  else if (pwLength != null || pwLength != ""){
-//     alert('Selection can not be left blank');
-//     starterInput ();
-} else {
+ } else {
     return pwLength;
 }
 }
 
 starterInput ();
 
-console.log(pwLength);
+
+// Replaced this function with 'While string inside passwordConditions()
+// Couldn't get it to work....
+// function userSelector() {
+//     let userResult = ' ';
+//     const usersLength = pwUserReq.length;
+//     for ( let i = 0; i < 1; i++ ) {
+//         userResult += pwUserReq.charAt(Math.floor(Math.random() * usersLength));
+//     }
+//     return userResult;
+// }
+
+
 
 function passwordConditions(){
   var pwUserReq = "";
+  // var pwCharSelect = "";
   lowercase = confirm("Would you like it to contain Lowercase?")
   uppercase = confirm("Would you like it to contain Uppercase?")
   numeric = confirm("Would you like it to contain Numbers?")
   specialChar = confirm("Would you like it to contain Special Characters?")
     
+
     if (lowercase){
-      pwUserReq += 'abcdefghijklmnopqrstuvwxyz'
+      userLowerCase = 'abcdefghijklmnopqrstuvwxyz'
+      pwUserReq += userLowerCase
+     if (pwUserReq){
+      var firstString = "";
+      while (firstString.length < 1) {
+        firstString += userLowerCase[Math.floor(Math.random() * userLowerCase.length)];
+      }
+      console.log(firstString);
+      }
+      
     }
     if (uppercase){
-      pwUserReq += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      userUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      pwUserReq += userUpperCase;
+     if (pwUserReq){
+      var secondString = "";
+      while (secondString.length < 1) {
+        secondString += userUpperCase[Math.floor(Math.random() * userUpperCase.length)];
+      }
     }
+    console.log(secondString);
+  }
     if (numeric){
-      pwUserReq += '0123456789'
+      userNumeric = '0123456789'
+      pwUserReq += userNumeric
+     if (pwUserReq){
+      var thirdString = "";
+      while (thirdString.length < 1) {
+        thirdString += userNumeric[Math.floor(Math.random() * userNumeric.length)];
+      }
+      }
+      console.log(thirdString)
     }
+
     if (specialChar){
-      pwUserReq += '!@#$%^&*";'
-    }
+      userSymbols = '!@#$%^&*";'
+      pwUserReq += userSymbols
+     if (pwUserReq){
+      var fourthString = "";
+      while (fourthString.length < 1) {
+        fourthString += userSymbols[Math.floor(Math.random() * userSymbols.length)];
+      }
+      }
+      console.log(fourthString)
+      }
   
   if (!lowercase && !uppercase && !numeric && !specialChar){
   alert('Please select at least ONE of the password criteria.')
@@ -72,15 +117,54 @@ function passwordConditions(){
 }
 
 
-    
+//The collection of user selections stored in a variable
 var output = passwordConditions();
 
-    console.log(lowercase);
-    console.log(uppercase);
-    console.log(numeric);
-    console.log(specialChar);
 
-    console.log(output);
+
+// WORKING PW GENERATOR
+function randomSelector(length) {
+    let result = ' ';
+    const charactersLength = output.length;
+    for ( let i = 4; i < length; i++ ) {
+        result += output.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+console.log('output');
+console.log(output);
+console.log('pwLength');
+console.log(randomSelector(pwLength))
+
+console.log("TO DO STILL:");
+console.log('1. need to concatenate all the outputs');
+console.log('2. need to apply output to the dom');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('password generated');
+
+
+
+
+
+
 
 
 
