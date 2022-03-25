b3b1eba11403fbba0b59215769c0718782d932fe
Kym Reilly <kymreilly@hotmail.com>
2022-03-25 20:30:11 +1030

    password length issue solved, redundant loops removed, requires more comments still

--- script.js
+++ script.js
@@ -25,125 +25,99 @@ var finalPw = "";
 var output
 
 
-function starterInput (){
-    pwLength = parseInt(prompt( 'How long would you like your password to be?', 'Min: 8  -  Max: 128'), 10);
- if (isNaN(pwLength)){
-    alert('Use only numbers please.');
-    starterInput ()
-} else if (pwLength < 8 || pwLength > 128) {
-    alert('Please select a another number! (Min: 8  -  Max: 128)');
-    starterInput ();
- } else {
-  return pwLength;
+function starterInput() {
+  pwLength = parseInt(
+    prompt(
+      "How long would you like your password to be?",
+      "Min: 8  -  Max: 128"
+    ),
+    10
+  );
 
-}
   console.log(pwLength);
-return pwLength;
+  if (isNaN(pwLength)) {
+    alert("Use only numbers please.");
+   starterInput();
+  } else if (pwLength < 8 || pwLength > 128) {
+    alert("Please select a another number! (Min: 8  -  Max: 128)");
+    starterInput();
+  }
+  console.log(pwLength);
+  return pwLength;
 }
-//<--------------------------------------question------------
-//  starterInput (); // need this insde of  passwordConditions
 
 
-
-// Replaced this function with 'While string inside passwordConditions()
-// Couldn't get it to work....
-// function userSelector() {
-//     let userResult = ' ';
-//     const usersLength = pwUserReq.length;
-//     for ( let i = 0; i < 1; i++ ) {
-//         userResult += pwUserReq.charAt(Math.floor(Math.random() * usersLength));
-//     }
-//     return userResult;
-// }
-
-
-
-function passwordConditions (){
+function passwordConditions() {
   oneEachChar = "";
   pwUserReq = "";
-  
 
-starterInput (); //TESTING THIS INSIDE THE FUNCTION
+  console.log(pwLength);
 
- console.log(pwLength);
+  lowercase = confirm("Would you like it to contain Lowercase?");
+  uppercase = confirm("Would you like it to contain Uppercase?");
+  numeric = confirm("Would you like it to contain Numbers?");
+  specialChar = confirm("Would you like it to contain Special Characters?");
 
-
-  lowercase = confirm("Would you like it to contain Lowercase?")
-  uppercase = confirm("Would you like it to contain Uppercase?")
-  numeric = confirm("Would you like it to contain Numbers?")
-  specialChar = confirm("Would you like it to contain Special Characters?")
-
-
-    if (lowercase){
-      userLowerCase = 'abcdefghijklmnopqrstuvwxyz'
-      pwUserReq += userLowerCase
-     if (pwUserReq){
-      var firstString = "";
-      while (firstString.length < 1) {
-        firstString += userLowerCase[Math.floor(Math.random() * userLowerCase.length)];
-      }
-      }
-      oneEachChar += firstString; 
-    }
-    if (uppercase){
-      userUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      pwUserReq += userUpperCase;
-     if (pwUserReq){
-      var secondString = "";
-      while (secondString.length < 1) {
-        secondString += userUpperCase[Math.floor(Math.random() * userUpperCase.length)];
-      }
-      
-    }
-    oneEachChar += secondString; 
+  if (!lowercase && !uppercase && !numeric && !specialChar) {
+    alert("Please select at least ONE of the password criteria.");
+    return;
   }
-    if (numeric){
-      userNumeric = '0123456789'
-      pwUserReq += userNumeric
-     if (pwUserReq){
-      var thirdString = "";
-      while (thirdString.length < 1) {
-        thirdString += userNumeric[Math.floor(Math.random() * userNumeric.length)];
-      }
-      }
-      oneEachChar += thirdString; 
-    }
 
-    if (specialChar){
-      userSymbols = '!@#$%^&*";'
-      pwUserReq += userSymbols
-     if (pwUserReq){
-      var fourthString = "";
-      while (fourthString.length < 1) {
-        fourthString += userSymbols[Math.floor(Math.random() * userSymbols.length)];
-      }
-      }
-      oneEachChar += fourthString; 
-      }
-  
-  if (!lowercase && !uppercase && !numeric && !specialChar){
-  alert('Please select at least ONE of the password criteria.')
-  passwordConditions()
-    
+  if (lowercase) {
+    userLowerCase = "abcdefghijklmnopqrstuvwxyz";
+    pwUserReq += userLowerCase;
+    var firstString = "";
+    firstString +=
+      userLowerCase[Math.floor(Math.random() * userLowerCase.length)];
+    oneEachChar += firstString;
   }
+
+  if (uppercase) {
+    userUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    pwUserReq += userUpperCase;
+    var secondString = "";
+    secondString +=
+      userUpperCase[Math.floor(Math.random() * userUpperCase.length)];
+
+    oneEachChar += secondString;
+  }
+
+  if (numeric) {
+    userNumeric = "0123456789";
+    pwUserReq += userNumeric;
+    var thirdString = "";
+    thirdString += userNumeric[Math.floor(Math.random() * userNumeric.length)];
+
+    oneEachChar += thirdString;
+  }
+
+  if (specialChar) {
+    userSymbols = '!@#$%^&*";';
+    pwUserReq += userSymbols;
+    var fourthString = "";
+    fourthString += userSymbols[Math.floor(Math.random() * userSymbols.length)];
+    oneEachChar += fourthString;
+  }
+  console.log(pwUserReq);
   return pwUserReq;
-
 }
-
 
 //The collection of user selections stored in a variable eg.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
 // var output = passwordConditions();
 
-
 // WORKING PW GENERATOR
 function randomSelector(length) {
+  var passLength = starterInput();
+
+  if (passLength >= 8 && passLength <= 128) {
     output = passwordConditions();
-    let result = ' ';
+    let result = " ";
     const charactersLength = output.length;
-    for ( let i = oneEachChar.length; i < length; i++ ) { //the loop I want : DYNAMIC
-        result += output.charAt(Math.floor(Math.random() * charactersLength));
+    for (let i = 0; i < passLength - oneEachChar.length; i++) {
+      result += output.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+  }
 }
 
 
@@ -157,6 +131,3 @@ function generatePassword() {
   finalPw = randomSelector(pwLength) + oneEachChar;
   return finalPw;
 };
-
-//QUESTION
-//PWLENGTH DOES NOT DISPLAY ON FIRST ALERT-INPUT LOOP BUT THEN WILL BE ADDED THEN AFTER
